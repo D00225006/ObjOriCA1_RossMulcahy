01c1d10d444780d0b5d0433f76feb1955bc20e4a
D00225006 <55512286+D00225006@users.noreply.github.com>
2019-10-21 10:31:39 +0100

    Add files via upload

--- circularShift.java
+++ circularShift.java
@@ -1,5 +1,7 @@
 package com.mycompany.objorica1_rossmulcahy;
 
+import static com.mycompany.objorica1_rossmulcahy.main.circularShiftRight;
+
 /**
  *
  * @author Rossm
@@ -8,18 +10,26 @@ public class circularShift
 {
     public void question4()
     {
-        int[] numbers = new int[5];
-        circularShiftRight(numbers);
+        int[] numbers = {1,2,3,4,5};
+        int shift = 2;
+        circularShiftRight(numbers, shift);
     }
     
-    public void circularShiftRight( int[] array)
-    {   
-        for(int i = 1; i < array.length; i++)
+    public void circularShiftRight( int[] array, int n)
+    {
+        if(n > array.length || n < 0)
+        {
+            System.out.println("Invalid shift value.");
+        }
+        else
+        {
+            for(int i = 1; i < array.length; i++)
         {
             int swapper = array[0];
             array[0] = array[i];
             array[i] = swapper;
         }
         System.out.println(array.toString());
+        }
     }
 }
