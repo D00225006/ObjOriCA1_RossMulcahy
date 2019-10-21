1c57598df4b1969158733a8633fa34662b18eae4
D00225006 <55512286+D00225006@users.noreply.github.com>
2019-10-21 11:02:54 +0100

    Changed Shifter
    Basic shifted would not work with the n value, far easier to create a new array with all values in the correct place, to then become the original array. Facing difficulty with While statement, may require reworking.

--- circularShift.java
+++ circularShift.java
@@ -11,25 +11,45 @@ public class circularShift
     public void question4()
     {
         int[] numbers = {1,2,3,4,5};
-        int shift = 2;
+        int shift = 1;
         circularShiftRight(numbers, shift);
     }
     
     public void circularShiftRight( int[] array, int n)
     {
+        int[] shiftedArray = new int[array.length];
+        
         if(n > array.length || n < 0)
         {
             System.out.println("Invalid shift value.");
         }
         else
         {
-            for(int i = 1; i < array.length; i++)
-        {
-            int swapper = array[0];
-            array[0] = array[i];
-            array[i] = swapper;
+            for(int i = 0; i < array.length; i++)
+            {
+                
+                while(i+n < array.length)
+                {
+                    array[i] = shiftedArray[i+n];
+                }
+                while(i+n > array.length)
+                {
+                    for(int j = 0; j < n; j++)
+                    {
+                        array[i] = shiftedArray[j];
+                    }
+                }
+            }
         }
-        System.out.println(array.toString());
+        
+        for(int i = 0; i < array.length; ++i)
+        {
+            System.out.print(array[i]);
+        }
+        System.out.println("---");
+        for(int i = 0; i < array.length; ++i)
+        {
+            System.out.print(shiftedArray[i]);
         }
     }
 }
